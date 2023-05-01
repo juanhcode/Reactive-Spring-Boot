94eb90d4c4c75f58f9aede1e4c4dc1d1af8c6950
Juan Hoyos <juanhoyos1347@gmail.com>
2023-04-30 19:24:14 -0500

    transformación

--- transformacion-combinacion-flujos/src/main/java/Ejemplo01.java
+++ transformacion-combinacion-flujos/src/main/java/Ejemplo01.java
@@ -1,11 +1,18 @@
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejemplo01 {
     public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
 
         Flux.fromArray(new String[]{"Tom","Melissa","Steve","Megan"})
                 .map(String::toUpperCase)
-                .subscribe(System.out::println);
+                .subscribe(lista::add);
+
+
+        lista.forEach(System.out::println);
 
     }
 }
